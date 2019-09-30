e98f8ab536d2590e3a66da3e1fdb49833799f008
Dominik Obuch <dominik.obuch@wp.pl>
2019-09-30 17:45:05 +0200

    final?

--- docs/js/article.js
+++ docs/js/article.js
@@ -1,14 +1,23 @@
 let lazyLoading = document.getElementsByClassName("lazy-load");
 
-window.addEventListener("scroll", () => {
+let slideLeftEl = document.getElementsByClassName("slideLeft");
+let slideRightEl = document.getElementsByClassName("slideRight");
 
+window.addEventListener("scroll", () => {
+    for(let i = 0; i<slideLeftEl.length; i++){
+        if(slideLeftEl[i].offsetTop< window.innerHeight + window.pageYOffset + 100){
+            slideLeftEl[i].classList.add("slide-from-left")
+        }
+    }
+    for(let i = 0; i<slideLeftEl.length; i++){
+        if(slideRightEl[i].offsetTop< window.innerHeight + window.pageYOffset + 100){
+            slideRightEl[i].classList.add("slide-from-right")
+        }
+    }
     for (let i = 0; i < lazyLoading.length; i++) {
-        console.log(lazyLoading[i].offsetTop, window.innerHeight + window.pageYOffset + 100)
-        if(lazyLoading[i].offsetTop < window.innerHeight + window.pageYOffset + 150){
+        if(lazyLoading[i].offsetTop < window.innerHeight + window.pageYOffset + 100){
             lazyLoading[i].classList.add("lazy-loading");
 
         }
     }
 })
-let lazyLoad = el =>{
-}
